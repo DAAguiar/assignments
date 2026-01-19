c7d865793e11107f54b010cab375cfaad68478ea
DAAguiar <diogo.aguiar@nos.pt>
2026-01-19 15:57:07 +0000

    added tests

--- life_expectancy/full_orchestration.py
+++ life_expectancy/full_orchestration.py
@@ -3,7 +3,7 @@ import argparse
 from pandas import DataFrame
 
 from life_expectancy.cleaning import LifeExpectancyOperations
-from life_expectancy.data_io import load_data, write_data
+from life_expectancy.data_io import load_data
 
 IMPORT_FILE_PATH = './life_expectancy/data/eu_life_expectancy_raw.tsv'
 
@@ -11,16 +11,12 @@ def life_expectancy_orchestration(country_code: str = 'PT',
                                   input_file_path = None) -> DataFrame:
     '''Performs whole orchestrantion pipeline for life_expectancy data.'''
     
-    
-    
     lifeExpectancyOperations = LifeExpectancyOperations(
         raw_df=load_data(input_file_path)
         ) # pylint: disable=invalid-name
 
     df_filtered = lifeExpectancyOperations.filter_region(country_code=country_code)
 
-    write_data(df_filtered, country_code)
-    
     return df_filtered
 
 
